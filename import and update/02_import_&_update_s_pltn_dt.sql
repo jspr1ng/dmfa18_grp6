ca1a708ad637321bb65b1ec30e6a03cfad8b3dd0
Jordan Springer <springer.131@osu.edu>
2018-11-25 19:15:45 -0500

    moved delete dupes code from G to S

--- import and update/02_import_&_update_s_pltn_dt.sql
+++ import and update/02_import_&_update_s_pltn_dt.sql
@@ -42,3 +42,18 @@ update s_pltn_dt
   set ID = null
   where id = ' '
 ;
+
+-- 3. remove duplicates
+delete t1
+  FROM `s_pltn_dt` AS t1
+  INNER JOIN `s_pltn_dt` AS t2
+ON t1.id = t2.id
+AND t1.DOB = t2.DOB
+AND t1.FUNDS_RAISED = t2.FUNDS_RAISED
+AND t1.GENDER = t2.GENDER
+AND t1.LIVING_PROOF = t2.LIVING_PROOF
+AND t1.RIDE_DISTANCE = t2.RIDE_DISTANCE
+AND t1.RIDE_YEARS = t2.RIDE_YEARS
+AND t1.RIDER_ID = t2.RIDER_ID
+AND t1.`YEAR` = t2.`YEAR`
+;
--- import and update/03_import_&_update_g_pltn_dt.sql
+++ import and update/03_import_&_update_g_pltn_dt.sql
@@ -30,16 +30,3 @@ update g_pltn_dt
 alter table g_pltn_dt
   drop column dob
 ;
--- 4. remove duplicates
-delete t1
-  FROM `g_pltn_dt` AS t1
-  INNER JOIN `g_pltn_dt` AS t2
-ON t1.id = t2.id
-AND t1.DOB = t2.DOB
-AND t1.FUNDS_RAISED = t2.FUNDS_RAISED
-AND t1.GENDER = t2.GENDER
-AND t1.LIVING_PROOF = t2.LIVING_PROOF
-AND t1.RIDE_DISTANCE = t2.RIDE_DISTANCE
-AND t1.RIDE_YEARS = t2.RIDE_YEARS
-AND t1.RIDER_ID = t2.RIDER_ID
-AND t1.`YEAR` = t2.`YEAR`
