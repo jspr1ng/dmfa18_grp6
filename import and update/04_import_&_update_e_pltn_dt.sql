334e8be30ce78705cb301b6701ada945ebed51d6
Jordan Springer <springer.131@osu.edu>
2018-11-25 20:15:57 -0500

    updated dob code

--- import and update/04_import_&_update_e_pltn_dt.sql
+++ import and update/04_import_&_update_e_pltn_dt.sql
@@ -2,7 +2,7 @@
 
 -- 1. create table e_pltn_dt
 CREATE TABLE e_pltn_dt
-  (id, exclusion_reason);
+  (id int, exclusion_reason varchar(max));
 
 -- 1. Load excluded ids and rationale
 
@@ -12,8 +12,14 @@ insert into e_pltn_dt
   FROM `pltn_dt` AS p1
   INNER JOIN `pltn_dt` AS p2
   ON p1.id = p2.`id`
-  WHERE p1.dob <> p2.`dob` 		-- 144 rows
-  AND p1.`gender` = p2.`gender` 	-- 144 rows
-  AND p1.rider_id = p2.rider_id 	-- 144 rows
-  --AND p1.`living_proof` = p2.`living_proof` -- dropped TO 0 ROWS
-  --AND p1.YEAR = p2.YEAR -- 0 ROWS
+  WHERE (
+    p1.dob <> p2.`dob` 		-- 144 rows
+    AND
+    p1.`gender` = p2.`gender` 	-- 144 rows
+        )
+  OR
+        (
+    p1.dob = p2.`dob` 		-- 144 rows
+    AND
+    p1.`gender` <> p2.`gender` 	-- 144 rows
+        )
