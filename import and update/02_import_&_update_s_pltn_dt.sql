fcbaba8082958bedaf1985efbd7b518a3d7def42
Jordan Springer <springer.131@osu.edu>
2018-11-25 20:16:20 -0500

    updated silver to remove delete dupe code

--- import and update/02_import_&_update_s_pltn_dt.sql
+++ import and update/02_import_&_update_s_pltn_dt.sql
@@ -2,11 +2,11 @@
 
 -- 1. create and load s_pltn_dt
 CREATE TABLE s_pltn_dt
-  (YEAR varchar, rider_id varchar, living_proof varchar, ride_distance smallint, funds_raised decimal(100,4), gender varchar, dob varchar, id int);
+  (YEAR varchar, rider_id varchar, living_proof varchar, ride_distance smallint, funds_raised decimal, gender varchar, dob varchar, id int);
 -- load from b_pltn_dt
 INSERT INTO s_pltn_dt
   SELECT YEAR, rider_id, living_proof, ride_distance, funds_raised, gender, dob, id
-  FROM `pltn_dt`;
+  FROM `b_pltn_dt`; --49,023
 
 -- 2. remove " " values
 update s_pltn_dt
@@ -22,14 +22,6 @@ update s_pltn_dt
   where living_proof = ' '
 ;
 update s_pltn_dt
-  set RIDE_DISTANCE	= NULL
-  where ride_distance = ' '
-;
-update s_pltn_dt
-  set FUNDS_RAISED = NULL
-  where funds_raised = ' '
-;
-update s_pltn_dt
 set GENDER
 ;
 update s_pltn_dt
@@ -38,13 +30,10 @@ set DOB
 update s_pltn_dt
 set RIDE_YEARS
 ;
-update s_pltn_dt
-  set ID = null
-  where id = ' '
-;
-
+/*
 -- 3. remove duplicates
-delete t1
+--delete t1
+SELECT t1.*
   FROM `s_pltn_dt` AS t1
   INNER JOIN `s_pltn_dt` AS t2
 ON t1.id = t2.id
@@ -53,7 +42,7 @@ AND t1.FUNDS_RAISED = t2.FUNDS_RAISED
 AND t1.GENDER = t2.GENDER
 AND t1.LIVING_PROOF = t2.LIVING_PROOF
 AND t1.RIDE_DISTANCE = t2.RIDE_DISTANCE
-AND t1.RIDE_YEARS = t2.RIDE_YEARS
-AND t1.RIDER_ID = t2.RIDER_ID
+--AND t1.RIDE_YEARS = t2.RIDE_YEARS
+--AND t1.RIDER_ID = t2.RIDER_ID
 AND t1.`YEAR` = t2.`YEAR`
-;
+;*/
