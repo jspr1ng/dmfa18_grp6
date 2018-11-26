4bbda5768358a2e67e6b99ab1fd6f1350e73f8c9
Jordan Springer <springer.131@osu.edu>
2018-11-25 20:00:01 -0500

    updated silver code

--- import and update/01_import_&_update_b_pltn_dt.sql
+++ import and update/01_import_&_update_b_pltn_dt.sql
@@ -2,9 +2,9 @@
 
 -- 1. create and load b_pltn_dt
 CREATE TABLE b_pltn_dt
-  (YEAR varchar, rider_id varchar, living_proof varchar, ride_distance smallint, funds_raised decimal, gender varchar, dob varchar, ride_years int, id int);
+  (YEAR varchar, living_proof varchar, ride_distance smallint, funds_raised decimal, gender varchar, dob varchar, id int);
 
 -- load from raw files (in this case, pre-combined raw files already loaded as a table)
 INSERT INTO b_pltn_dt
-  SELECT YEAR, rider_id, living_proof, ride_distance, funds_raised, gender, dob, ride_years, id
-  FROM `pltn_dt`;
+  SELECT YEAR, living_proof, ride_distance, funds_raised, gender, dob, id
+  FROM `pltn_dt`;-- 49,023
